2ff55be47d276c1443451cec38ea0d0310c9df61
John Gomersall <thegoms@gmail.com>
2024-08-23 10:09:55 +0100

    Load the new account theme for reference
    Signed-off-by: John Gomersall <thegoms@gmail.com>

---
+++ theme/keycloak.v3/account/resources/assets/formatDate-QNS74n8b.js
@@ -0,0 +1,2 @@
+import{n}from"./main-oR2vHPRf.js";const e={dateStyle:"long"},a={timeStyle:"short"},r={...e,...a};function T(t,o=r){return t.toLocaleString(n.languages,o)}export{T as f};
+//# sourceMappingURL=formatDate-QNS74n8b.js.map
