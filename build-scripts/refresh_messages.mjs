18f7da8af5327ec5fe81c2882a6e61d916f08e9d
John Gomersall <thegoms@gmail.com>
2024-08-23 12:36:41 +0100

    Dummy language xx for seeing missed translations
    Signed-off-by: John Gomersall <thegoms@gmail.com>

--- build-scripts/refresh_messages.mjs
+++ build-scripts/refresh_messages.mjs
@@ -47,3 +47,25 @@ for (const [code, keycloakMessages] of Object.entries(allKeycloakMessages)) {
     }
     writeFileSync(existingMessageFile, existingMessages.join('\n'));
 }
+
+// Create a test message file that includes all keycloak messages marked with * if we haven't listed them
+const xxMessages = ['# The following are used for translation test purposes',
+    '# If items surrounded by ** appear in the UI then we need to add that property into the',
+    '# messages_en file so that translations can be done in Crowdin'];
+
+// First add all the messages we've identified
+for (const message of enMessages) {
+    const parts = message.split('=');
+    xxMessages.push(parts.length > 1 ? `${parts[0]}=[${parts[0]}]` : message);
+}
+
+// Then add any keycloak ones that we don't already list
+xxMessages.push("# The following Keycloak messages won't be picked up by Crowdin");
+for (const message of allKeycloakMessages.en) {
+    const parts = message.split('=');
+    if (parts.length < 2) continue;
+    const messageSearch = `${parts[0]}=`;
+    if (!enMessages.find(m => m.startsWith(messageSearch))) xxMessages.push(`${parts[0]}=[*${parts[0]}*]`);
+}
+
+writeFileSync('theme/off/common/messages/messages_xx.properties', xxMessages.join('\n'));
