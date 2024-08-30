01f3e5be4fa8a8a7eba7a16f070ba1b36c0f8c1b
John Gomersall <thegoms@gmail.com>
2024-08-30 17:20:18 +0100

    Fix dropdown on country field
    Signed-off-by: John Gomersall <thegoms@gmail.com>

--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from "@playwright/test";
-import { HELPER_TEXT } from "./expected-styles";
+import { HELPER_TEXT, INPUT_FIELD } from "./expected-styles";
 import { gotoHome, matchStyles, registerLink } from "./test-helper";
 
 test("registration page", async ({ page }) => {
@@ -12,7 +12,9 @@ test("registration page", async ({ page }) => {
   const afterHelper = page.locator('.pf-c-form__helper-text-after:has-text("username"):below(#username)');
   expect(await matchStyles(afterHelper, HELPER_TEXT)).toBeNull();
 
+  // Check dropdown icons are being displayed
+  await expect(page.locator('.pf-v5-c-form-control__toggle-icon:near(#login-select-toggle)')).toBeVisible();
+  await expect(page.locator('.pf-v5-c-form-control__toggle-icon:near(#country)')).toBeVisible();
 
-  // TODO: Drop-down icon visible on country select
   // TODO: Countries are sorted by localized value
 });
--- theme/off/login/user-profile-commons.ftl
+++ theme/off/login/user-profile-commons.ftl
@@ -177,6 +177,14 @@
 		<option value="${option}" <#if attribute.values?seq_contains(option)>selected</#if>><@selectOptionLabelText attribute=attribute option=option/></option>
 	</#list>
 	</select>
+	<span class="pf-v5-c-form-control__utilities">
+		<span class="pf-v5-c-form-control__toggle-icon">
+		<svg class="pf-v5-svg" viewBox="0 0 320 512" fill="currentColor" aria-hidden="true" role="img" width="1em" height="1em">
+			<path d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z">
+			</path>
+		</svg>
+		</span>
+	</span>
 </#macro>
 
 <#macro inputTagSelects attribute>
