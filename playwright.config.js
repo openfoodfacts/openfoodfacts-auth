6ba9fc925c24bfcb7375722b1480d64151a33518
John Gomersall <thegoms@gmail.com>
2024-10-29 09:21:29 +0000

    Temporary config changes while testing
    Signed-off-by: John Gomersall <thegoms@gmail.com>

--- playwright.config.js
+++ playwright.config.js
@@ -20,7 +20,9 @@ module.exports = defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  //retries: process.env.CI ? 2 : 0,
+  // Temporarily suspend retries
+  retries: 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -28,7 +30,9 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    //trace: 'on-first-retry',
+    // Temporarily capture every trace
+    trace: 'on',
   },
 
   /* Configure projects for major browsers */
