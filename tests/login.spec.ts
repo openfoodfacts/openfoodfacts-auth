132c6e4c18d2c0b29fb5613887de8e263a2d9da1
John Gomersall <thegoms@gmail.com>
2025-01-08 09:58:03 +0000

    Failing test to redirect main page after email verification
    Signed-off-by: John Gomersall <thegoms@gmail.com>

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -77,18 +77,24 @@ test("locale from app is respected", async ({ page }) => {
   
   const message = await getLastEmail(userName);
   expect(message.plaintext).toContain('^emailVerificationBody 0=');
-  await clickEmailVerifyLink(page, message);
 
-  await expect(page.getByLabel('preferred_username')).toHaveValue(userName);
-  expect(page.url()).toContain('lang=xx');
+  // Open a new tab to verify the email
+  const verifyPage = await page.context().newPage();
+  await clickEmailVerifyLink(verifyPage, message);
+
+  // Login should occur on the original page and on the verify page
+  for (const testPage of [verifyPage, page]) {
+    await expect(testPage.getByLabel('preferred_username')).toHaveValue(userName);
+    expect(testPage.url()).toContain('lang=xx');
+  
+    await testPage.getByRole("button", { name: "Account" }).click();
+    await expect(testPage.getByText('^personalInfoDescription^')).toBeVisible();
+  }
 
   const myMessage2 = await redisClient.getMessageForUser(userName);
   expect(myMessage2).toBeTruthy();
-
-  await page.getByRole("button", { name: "Account" }).click();
-  await expect(page.getByText('^personalInfoDescription^')).toBeVisible();
-
-  // Fetch the user via API and make sure locale is set correctly
+  
+    // Fetch the user via API and make sure locale is set correctly
   const headers = await getKeycloakHeaders();
   const users = await (await fetch(`${keycloakUserUrl}?exact=true&username=${userName}`, {headers})).json();
   expect(users[0].attributes.locale[0]).toBe('xx');
