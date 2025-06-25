442eec75c668981f794ab2718f87304de20ec6a8
John Gomersall <thegoms@gmail.com>
2025-06-25 17:08:53 +0100

    Add verbose flag during test container build
    Signed-off-by: John Gomersall <thegoms@gmail.com>

--- runtime-scripts/build.sh
+++ runtime-scripts/build.sh
@@ -1,4 +1,4 @@
 echo "$(date -u) *** Starting keycloak in configure test mode ***"
-/opt/keycloak/bin/kc.sh start --optimized --http-enabled=true --cache=local &
+/opt/keycloak/bin/kc.sh start --optimized --http-enabled=true --cache=local --verbose --spi-theme-static-max-age=-1 --spi-theme-cache-themes=false --spi-theme-cache-templates=false &
 # Run the after_setup script in dev mode so that test clients are created and persisted in the test image
 KEYCLOAK_STARTUP=dev sh /opt/off/after_startup.sh
